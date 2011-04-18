c4bb0090e10d1fd269242bc257771dda14867e6a
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-04-18 18:34:02 +0100

    correctly install ircbot script

--- src/bin/ircbot.php
+++ src/bin/ircbot.php
@@ -1,5 +1,46 @@
 #!/usr/bin/env php
 <?php
+/**
+ * Copyright (c) 2011 b3cft
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Gradwell dot com Ltd nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    b3cft
+ * @subpackage IrcBot
+ * @author     Andy 'Bob' Brockhurst, <andy.brockhurst@b3cft.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://b3cft.github.com/phpIrcBot
+ * @version    @@PACKAGE_VERSION@@
+ */
+
 namespace b3cft\IrcBot;
 use b3cft\CoreUtils\Config,
     b3cft\CoreUtils\Registry;
@@ -10,8 +51,8 @@ if (false === empty($devPath))
     __gwc_autoload_alsoSearch($devPath);
 }
 
-/* Load the default config */
-Config::getInstance()->loadIniFile('../data/ircbot.ini');
+/* Default config should none be provide on command line */
+define('DEFAULT_CONFIG', '@@DATA_DIR@@/IRCBot/ircbot.ini');
 
 /* Register the config object in registry */
 Registry::getInstance()->register('Config', Config::getInstance());
