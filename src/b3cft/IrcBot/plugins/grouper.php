a0bf165faa7f08dc575283d6e8fc6156d72ff078
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2012-01-07 10:42:54 +0000

    Implement grouper plugin requested feature. Fix #6

---
+++ src/b3cft/IrcBot/plugins/grouper.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Copyright (c) 2011 b3cft
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Gradwell dot com Ltd nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    b3cft
+ * @subpackage IrcBot
+ * @author     Andy 'Bob' Brockhurst, <andy.brockhurst@b3cft.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://github.com/b3cft
+ * @version    @@PACKAGE_VERSION@@
+ */
+
+use b3cft\IrcBot\ircMessage,
+    b3cft\IrcBot\ircConnection;
+
+class grouper extends b3cft\IrcBot\ircPlugin
+{
+
+    protected $groups = array();
+
+    /**
+     * Constructor, do normal construct, then retreive state from disk
+     *
+     * @param ircConnection $client - ircConnection client for communication
+     * @param mixed[]       $config - configuration for this plugin
+     *
+     * @return welcomer
+     */
+    public function __construct(ircConnection $client, array $config)
+    {
+        parent::__construct($client, $config);
+        $this->retrieveData();
+    }
+
+    /**
+     * Persist state to disk on destruct
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        $this->persistData();
+    }
+
+    /**
+     * Retrieve state from disk
+     *
+     * @return void
+     */
+    private function retrieveData()
+    {
+        if (false !== realpath($this->config['datafile']) &&
+            true === is_file($this->config['datafile']) &&
+            true === is_readable($this->config['datafile'])
+            )
+        {
+            $file = file_get_contents($this->config['datafile']);
+            $data = unserialize(gzuncompress($file));
+            foreach ($data as $key=>$values)
+            {
+                $this->$key = $values;
+            }
+        }
+    }
+
+    /**
+     * Persist state to disk
+     *
+     * @return void
+     */
+    private function persistData()
+    {
+        if (false === is_null($this->config['datafile']) &&
+            false === is_dir($this->config['datafile'])
+            )
+        {
+            $data = array(
+                'groups' => $this->groups,
+                );
+            file_put_contents($this->config['datafile'], gzcompress(serialize($data)));
+        }
+    }
+
+    /**
+     * Process a message
+     *
+     * @param ircMessage $message - message to process
+     *
+     * @return void
+     */
+    public function process(ircMessage $message)
+    {
+        $messageParts = explode(' ', $message->message);
+        if (1 < count($messageParts))
+        {
+            $command = $messageParts[0];
+            $params  = array_slice($messageParts, 1);
+        }
+        else
+        {
+            $command = $message->message;
+            $params  = array();
+        }
+        $command = trim($command, ':');
+        if ('PRIVMSG' === $message->action && true === $message->isToMe)
+        {
+            if ('group' === $command && true === empty($params) && false === empty($this->groups))
+            {
+                $groups = implode(', ', array_keys($this->groups));
+                $this->client->writeline("PRIVMSG $message->channel :Groups available: $groups");
+            }
+            else if (
+                'group' === $command &&
+                false === empty($params) &&
+                1 === count($params) &&
+                false === empty($this->groups[$params[0]])
+                )
+            {
+                unset($this->groups[$params[0]]);
+                $this->persistData();
+            }
+            else if ('group' === $command && false === empty($params))
+            {
+                $group = array_shift($params);
+                array_walk(
+                    $params,
+                    function(&$item, $key)
+                    {
+                        $item = trim($item, ':, ');
+                    }
+                );
+                $this->groups[$group] = $params;
+                $this->persistData();
+            }
+        }
+        else if ('PRIVMSG' === $message->action && false === empty($this->groups[$command]))
+        {
+            $group = implode(', ', $this->groups[$command]);
+            $this->client->writeline("PRIVMSG $message->channel :^^ $group");
+        }
+    }
+
+    /**
+     * Return a list of commands the plugin responds to
+     *
+     * @return string[]
+     */
+    public function getCommands()
+    {
+        return array('group');
+    }
+}
---
+++ src/tests/plugins/grouperTest.php
@@ -0,0 +1,258 @@
+<?php
+/**
+ * Copyright (c) 2011 b3cft
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Gradwell dot com Ltd nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    b3cft
+ * @subpackage IrcBot
+ * @author     Andy 'Bob' Brockhurst, <andy.brockhurst@b3cft.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://b3cft.github.com/phpIrcBot
+ * @version    @@PACKAGE_VERSION@@
+ */
+
+use b3cft\IrcBot\ircMessage;
+
+/* Include PSR0 Autoloader and add dev path to search */
+if (false === defined('PSR0AUTOLOADER'))
+{
+    include_once 'gwc.autoloader.php';
+    $devPath = realpath(dirname(__FILE__).'/../../');
+    if (false === empty($devPath))
+    {
+        __gwc_autoload_alsoSearch($devPath);
+    }
+    define('PSR0AUTOLOADER', true);
+}
+if (false === defined('PRS0PLUGINS'))
+{
+    define('PHP_DIR', '@@PHP_DIR@@');
+    if (PHP_DIR !== '@@'."PHP_DIR".'@@')
+    {
+        __gwc_autoload_alsoSearch(PHP_DIR.'/b3cft/IRCBot/plugins');
+    }
+    $devPath = realpath(dirname(__FILE__).'/../../b3cft/IrcBot/plugins/');
+    if (false === empty($devPath))
+    {
+        __gwc_autoload_alsoSearch($devPath);
+    }
+    define('PRS0PLUGINS', true);
+}
+
+/**
+ *
+ *
+ * @author b3cft
+ */
+class grouperTest extends PHPUnit_Framework_TestCase
+{
+    private $client;
+    private $config;
+
+    /**
+     * Create mock ircConnection and configs for each test
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->config = array(
+            'enabled'  => 'true'
+        );
+        if (PHP_DIR === '@@'."PHP_DIR".'@@')
+        {
+            $this->config['datafile'] = __DIR__.DIRECTORY_SEPARATOR.'fixtures'.DIRECTORY_SEPARATOR;
+        }
+        else
+        {
+            $this->config['datafile'] = '/tmp/';
+        }
+
+        $this->client = $this->getMock(
+            'b3cft\IrcBot\ircConnection',
+            array('writeline', 'getMessageStack', '__destruct'), array(), '', false
+        );
+    }
+
+    /**
+     * Test get commands method returns correct list of commands we support
+     *
+     * @return void
+     */
+    public function testGetCommands()
+    {
+        $this->config['datafile'] = null;
+
+        $plugin = new grouper($this->client, $this->config);
+
+        $keys = array_flip($plugin->getCommands());
+        $this->assertArrayHasKey('group', $keys);
+    }
+
+    /**
+     * Test grouper responses
+     *
+     * @param string $message   - message to respond to
+     * @param mixed  $responses - expected repsonses
+     *
+     * @dataProvider responsesDataProvider
+     * @group        dataprovider
+     *
+     * @return void
+     */
+    public function testResponses($message, $responses)
+    {
+        $this->config['datafile'] .= 'fixtures-responses';
+
+        if (
+            true === is_null($message) &&
+            true === is_null($responses) &&
+            true === file_exists($this->config['datafile'])
+            )
+        {
+            unlink($this->config['datafile']);
+            $this->assertFileNotExists(
+                $this->config['datafile'],
+                'fixtures not cleared'
+            );
+            return;
+        }
+
+        $plugin = new grouper($this->client, $this->config);
+
+        if (true === is_null($responses))
+        {
+            $this->client->expects($this->never())
+                ->method('writeline');
+        }
+        else if (is_string($responses))
+        {
+            $this->client->expects($this->once())
+                ->method('writeline')
+                ->with($responses);
+        }
+        else if (true === is_array($responses))
+        {
+            foreach ($responses as $index => $response)
+            {
+                $this->client->expects($this->at($index))
+                    ->method('writeline')
+                    ->with($response);
+            }
+        }
+
+        $plugin->process(new ircMessage($message, 'unit'));
+    }
+
+    /**
+     * Data provider for testResponses
+     *
+     * @return mixed[]
+     */
+    public function responsesDataProvider()
+    {
+        return array(
+            array(
+                null,
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupa Please could you look at this.',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit: group',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG unit :group',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit group groupa two three',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupa Please could you look at this.',
+                'PRIVMSG #test :^^ two, three',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG unit :group',
+                'PRIVMSG one :Groups available: groupa',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit: group',
+                'PRIVMSG #test :Groups available: groupa',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG unit :group groupb two, three:, four',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG unit :group groupa two',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit: group',
+                'PRIVMSG #test :Groups available: groupa, groupb',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupa: Please could you look at this.',
+                'PRIVMSG #test :^^ two',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupb Please could you look at this.',
+                'PRIVMSG #test :^^ two, three, four',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit: group groupa three',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupa: Please could you look at this.',
+                'PRIVMSG #test :^^ three',
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :unit: group groupa',
+                null,
+            ),
+            array(
+                ':one!one@1.2.3 PRIVMSG #test :groupa: Please could you look at this.',
+                null,
+            ),
+            array(
+                null,
+                null,
+            ),
+        );
+    }
+}
